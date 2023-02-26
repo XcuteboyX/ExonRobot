cb387655ef6830eb86256730a81f1a9d4de8fd9c
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-26 11:40:24 +0530

    Add files via upload

---
+++ Exon/modules/repo.py
@@ -0,0 +1,41 @@
+# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
+# TG :- @Abishnoi1M
+#     MY ALL BOTS :- Abishnoi_bots
+#     GITHUB :- KingAbishnoi ""
+
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from Exon import Abishnoi as pbot
+
+ABISHNOIX = "https://te.legra.ph/file/6d41f79669d8914b30f53.mp4"
+
+
+@pbot.on_message(filters.command("repo"))
+async def repo(_, message):
+    await message.reply_photo(
+        photo=ABISHNOIX,
+        caption=f"""✨ **ʜᴇʏ {message.from_user.mention},**
+
+**ᴏᴡɴᴇʀ  : [ᴀɴᴜᴊ🤧](https://t.me/its_cute_babu)**
+**ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{y()}`
+**ʟɪʙʀᴀʀʏ ᴠᴇʀꜱɪᴏɴ :** `{o}`
+**ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{s}`
+**ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{z}`
+** ᴇɴᴊᴏʏ**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "•sᴏᴜʀᴄᴇ 🤡•", url="https://t.me/X_TEAM_IMMORTALS_X"
+                    ),
+                ]
+            ]
+        ),
+    )
